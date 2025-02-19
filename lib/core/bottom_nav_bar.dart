865df2f37e70c2e95cb9dc465bad1e036ef4801e
NDuc <nguyenngocduc260504@gmail.com>
2025-02-19 21:24:00 +0700

    update scaffold and bottom bar

--- lib/core/bottom_nav_bar.dart
+++ lib/core/bottom_nav_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:snaeaker_store/screens/home_screen.dart';
 
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
@@ -11,18 +12,28 @@ class BottomNavBar extends StatefulWidget {
 class _BottomNavBarState extends State<BottomNavBar> {
 
   final appScreens = [
-    const Text("Home"),
-    const Text("Search"),
-    const Text("Cart"),
-    const Text("Profile"),
+    const HomeScreen(),
+    Center(child: const Text("Search")),
+    Center(child: const Text("Cart")),
+    Center(child: const Text("Profile")),
   ];
+
+  //change index for BottomNavigationBarItem
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Center(child: Text("My Tickets"))),
-      body: Center(child: appScreens[0]),
+      appBar: AppBar(title: Center(child: Text("Sneaker Store"))),
+      body: appScreens[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
         unselectedItemColor: Colors.grey,
         selectedItemColor: const Color.fromARGB(255, 54, 54, 54),
         showSelectedLabels: false,
@@ -35,13 +46,13 @@ class _BottomNavBarState extends State<BottomNavBar> {
 
           BottomNavigationBarItem(
             icon: Icon(FontAwesomeIcons.magnifyingGlass),
-            activeIcon: Icon(FontAwesomeIcons.magnifyingGlassMinus),
+            activeIcon: Icon(FontAwesomeIcons.magnifyingGlassPlus),
             label: "Search",
           ),
 
           BottomNavigationBarItem(
             icon: Icon(FontAwesomeIcons.bagShopping),
-            activeIcon: Icon(FontAwesomeIcons.cartPlus),
+            activeIcon: Icon(FontAwesomeIcons.bagShopping),
             label: "Cart",
           ),
 
