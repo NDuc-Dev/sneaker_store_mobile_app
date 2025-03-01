1dcf0cc34340972002740202763c31f125ae7d12
NDuc <nguyenngocduc260504@gmail.com>
2025-03-01 17:48:39 +0700

    update newest product widget

--- lib/screens/widgets/new_product.dart
+++ lib/screens/widgets/new_product.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:snaeaker_store/core/res/media.dart';
 import 'package:snaeaker_store/core/res/styles/app_style.dart';
 
@@ -14,6 +15,14 @@ class NewProduct extends StatelessWidget {
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.1),
+            blurRadius: 5,
+            spreadRadius: 0.5,
+            offset: const Offset(0, 2),
+          ),
+        ],
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -30,8 +39,65 @@ class NewProduct extends StatelessWidget {
             ),
           ),
           SizedBox(height: 10),
-          Text(
-            "Nike", style: AppStyles.textStyle2),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 13),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Nike",
+                  style: AppStyles.textStyle2.copyWith(
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 3, vertical: 2),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(6),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withValues(alpha: 0.1),
+                        blurRadius: 5,
+                        spreadRadius: 0.5,
+                        offset: const Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Baseline(
+                        baseline: 6,
+                        baselineType: TextBaseline.alphabetic,
+                        child: Icon(
+                          FontAwesomeIcons.solidStar,
+                          color: Colors.amber,
+                          size: 10,
+                        ),
+                      ),
+                      SizedBox(width: 5),
+                      Text(
+                        "4.5",
+                        style: AppStyles.textStyle2.copyWith(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(left: 13),
+            child: Text(
+              "Nike Air Force 1",
+              style: AppStyles.headLineStyle2.copyWith(fontSize: 18),
+            ),
+          ),
         ],
       ),
     );
